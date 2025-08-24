739843b5a688c56cbdcf4566d14118d6858f0383
Monae <93678860+purplemo340@users.noreply.github.com>
2025-08-23 20:36:13 -0400

    removed lines for giyhub pages

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
- base: "/purplemo340.github.io", plugins: [react()],
- nested: "proj"
+export default defineConfig({ 
+    plugins: [react()],
 })
